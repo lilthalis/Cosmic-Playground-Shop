2bbbc29e30df06540a78a7fdcbaa212e55c03878
Lil Thalis <jacomossi.thalison@escola.pr.gov.br>
2026-02-26 09:01:12 -0300

    index

--- script.js
+++ script.js
@@ -60,3 +60,13 @@ document.addEventListener('DOMContentLoaded', () => {
         setTimeout(() => banner.classList.add('show'), 2000);
     }
 });
+
+// Navegação Suave (Smooth Scroll)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
